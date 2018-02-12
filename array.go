ebe293d18c13d6b24741b3ec34a2039bb7cfe34b
gomachan46 <shiro.gomachan46@gmail.com>
2018-02-12 19:17:24 +0900

    array

---
+++ array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 配列の長さは型の一部分なので配列のサイズを変えることはできない
+func main() {
+	var a [2]string
+	a[0] = "hello"
+	a[1] = "world"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
