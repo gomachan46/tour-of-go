3a25e2d6fb5a27d16eb3834b7e2a59bf2c95f8e9
gomachan46 <shiro.gomachan46@gmail.com>
2018-02-26 02:29:26 +0900

    interface values

---
+++ interface-values.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
+// インタフェースの値は、値と具体的な方のタプルと考えることができる
+// →具体的な型の値を保持する
+func main() {
+	var i I
+
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
